b983b67a36a3a8d36d9031445a7327d275184b50
JULIAN AKSELRAD <47165897@ort>
2022-08-23 09:04:34 -0300

    Models

---
+++ Models/Pozo.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace TP7_Bursztyn_Witlis_Akselrad.Models
+{
+    public class pozo
+    {
+        private int _importe;
+        private bool _valorSeguro;
+
+         public pozo (int importe, bool valorSeguro)
+        {
+            _importe = importe;
+            _valorSeguro = valorSeguro;
+        }
+    }
+}
---
+++ Models/juegoQQSM.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace TP7_Bursztyn_Witlis_Akselrad.Models
+{
+    public static class juegoQQSM
+    {
+        private static int _preguntaActual;
+        private static char _respuestaCorrectaActual;
+        private static int _posicionPozo;
+        private static int _pozoAcumuladoSeguro; 
+        private static int _pozoAcumulado;
+        private static bool _comodin5050;
+        private static bool _comodinDobleChance;
+        private static bool _comodinSaltearPregunta;
+        private static List<pozo> _ListaPozo;
+        private static Jugador _player;
+
+        public static void IniciarJuego(string nombre, DateTime FechaHora)
+        {
+            _preguntaActual=1;
+            _respuestaCorrectaActual=0;
+            _posicionPozo=1;
+            _pozoAcumuladoSeguro=5000;
+            _pozoAcumulado=0;
+            _comodin5050=true;
+            _comodinDobleCHance=true;
+            _comodinSaltearPregunta=true;
+            
+             string sql="INSERT INTO Jugadores(idJugador, Nombre, FechaHora) VALUES(@idJugador, @Nombre, @FechaHora)";
+        }
+
+    }
+}
