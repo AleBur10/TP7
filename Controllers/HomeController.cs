2ac78a6dac61c9e819bf8ecce55654a2c89ee0c0
ALEXIS BURSZTYN <46878021@ort>
2022-09-15 16:34:35 -0300

    Segui con los models y con el controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,8 +26,20 @@ public class HomeController : Controller
     {
         juegoQQSM.IniciarJuego(Nombre);
         ViewBag.Pregunta = juegoQQSM.ObtenerProximaPregunta();
-        ViewBag.Respuestas = juegoQQSM.ObtenerRespuestas();
+        ViewBag.Respuesta = juegoQQSM.ObtenerRespuestas();
         return View();
+    }
+    public IActionResult PreguntaRespondida(char opcion, char opcionComodin)
+    {
+        var acierto = juegoQQSM.respuestaUsuario(opcion, opcionComodin);
+        if (acierto == true)
+        {
+            return View("RespuestapreguntaOk");
+        }
+        else
+        {
+            return View("PantallaFindelJuego");
+        }
     }
     public IActionResult PantallaFindelJuego()
     {
@@ -41,44 +53,9 @@ public class HomeController : Controller
     {
         return View();
     }
-  //  [HttpGet]
-    /*public IActionResult PreguntaRespondida(char Opcion1, char Opcion2, char Opcion3, char Opcion4)
-    {
-        ViewBag.Opciones = juegoQQSM.ObtenerRespuestas(Opcion1, Opcion2, Opcion3, Opcion4);
-        if(Opcion1 != null){
-           return View("RespuestapreguntaOK");
-        }
-        else
-        {
-           return View("PantallaFindelJuego");
-        }
-        if(Opcion2 != null){
-           return View("RespuestapreguntaOK");
-        }
-        else
-        {
-           return View("PantallaFindelJuego");
-        }
-        if(Opcion3 != null){
-           return View("RespuestapreguntaOK");
-        }
-        else
-        {
-           return View("PantallaFindelJuego");
-        }
-        if(Opcion4 != null){
-           return View("RespuestapreguntaOK");
-        }
-        else
-        {
-           return View("PantallaFindelJuego");
-        }
-    }*/
-    // public IActionResult FinDelJuego()
-    // {
-    //     ViewBag.DetalleJugador = juegoQQSM.verInfoJugador(IdJugador);
-    //     return View();
-    // }
+
+
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
--- Models/juegoQQSM.cs
+++ Models/juegoQQSM.cs
@@ -8,7 +8,7 @@ namespace TP7_Bursztyn_Witlis_Akselrad.Models
 {
     public static class juegoQQSM
     {
-        private static string _connectionString = "Server=A-PHZ2-CIDI-038;DataBase=JuegoQQSM;Trusted_Connection=true;";
+        private static string _connectionString = "Server=A-PHZ2-CIDI-003;DataBase=JuegoQQSM;Trusted_Connection=true;";
         private static int _preguntaActual;
         private static char _respuestaCorrectaActual;
         private static int _posicionPozo;
@@ -31,10 +31,10 @@ namespace TP7_Bursztyn_Witlis_Akselrad.Models
             _comodin5050 = true;
             _comodinDobleChance = true;
             _comodinSaltearPregunta = true;
-            _ListaPozo.Add(new pozo(1000,false));
-            _ListaPozo.Add(new pozo(2000,false));
-            _ListaPozo.Add(new pozo(5000,false));
-            _ListaPozo.Add(new pozo(10000,true));
+            _ListaPozo.Add(new pozo(1000, false));
+            _ListaPozo.Add(new pozo(2000, false));
+            _ListaPozo.Add(new pozo(5000, false));
+            _ListaPozo.Add(new pozo(10000, true));
             /*_ListaPozo.Add(20000);
             _ListaPozo.Add(30000);
             _ListaPozo.Add(50000);
@@ -78,70 +78,85 @@ namespace TP7_Bursztyn_Witlis_Akselrad.Models
                 string sql = "SELECT * FROM RESPUESTAS WHERE idPregunta= @pIdPregunta";
                 listaRespuestas = bd.Query<Respuesta>(sql, new { pIdPregunta = _preguntaActual }).ToList();
             }
-            return listaRespuestas;
-        }
-        /*
-        public bool respuestaUsuario(char opcion, char opcionComodin = ' ')
-        {
-            bool acierto = false;
-            if (opcion != null && opcionComodin != null)
+            foreach (Respuesta item in listaRespuestas)
             {
-                using (sqlConnection bd = new sqlConnection(_connectionString))
+                if (item.Correcta == true)
                 {
-                    string sql = "UPDATE Jugador SET ComodinDobleChance = False WHERE idJugador = @_idJugador";
-                    bd.Execute;
+                    _respuestaCorrectaActual = item.OpcionRespuesta;
                 }
             }
-            if (opcion == _respuestaCorrectaActual)
+            return listaRespuestas;
+        }
+        public static bool respuestaUsuario(char opcion, char opcionComodin = 'j')
+        {
+            bool acierto = false;
+            // Valida si se uso el comodin. Si no lo uso todavia, lo cambia en la db. Si ya lo habia usado, no se toman las rtas como corercta 
+            if (_comodinDobleChance == true && opcionComodin != 'j') // Si todavia no uso el comodin.
             {
-                if (_ListaPozo[_posicionPozo]._valorSeguro == true)
+                _comodinDobleChance = false;
+                var id = _player.IdJugador;
+                using (SqlConnection bd = new SqlConnection(_connectionString))
                 {
-                    _pozoAcumuladoSeguro == _posicionPozo;
+                    string sql = "UPDATE Jugadores SET ComodinDobleChance=0 WHERE idJugador = @pidJugador";
+                    bd.Execute(sql, new { pidJugador = id });
                 }
-                _posicionPozo++;
-                acierto = true;
+            }
+            else
+            {
+                if (opcion == _respuestaCorrectaActual || opcionComodin == _respuestaCorrectaActual) //si las opciones que manda el usuario son correctas
+                {
+                    if (_ListaPozo[_posicionPozo].ValorSeguro == true)
+                    {
+                        _pozoAcumuladoSeguro += _ListaPozo[_posicionPozo].Importe;
+                    }
+                    _posicionPozo++;
+                    acierto = true;
+                }
             }
             return acierto;
         }
-        
-        public static List<Pozo> ListarPozo()
-        {
-            return _ListaPozo;
-        }
-        public static int DevolverPosicionPozo(int _posicionPozo)
-        {
-            return _posicionPozo;
-        }
-        public static char[] Descartar50()
-        {
-            if (Comodin50 == true)
-            {
-                using (SqlConnection bd = new SqlConnection(_connectionString))
-                {
-                    string sql = "UPDATE jugador SET comodin50 = false WHERE idJugador = @_idJugador";   
-
-                }
+        //incrementar posicion pozo. Si tiene pozo seguro recibe la guita del pozo seguro actual
 
 
 
+        /*  public static List<Pozo> ListarPozo()
+         {
+             return _ListaPozo;
+         }
+         public static int DevolverPosicionPozo(int _posicionPozo)
+         {
+             return _posicionPozo;
+         }
+         public static char[] Descartar50()
+         {
+             if (Comodin50 == true)
+             {
+                 using (SqlConnection bd = new SqlConnection(_connectionString))
+                 {
+                     string sql = "UPDATE jugador SET comodin50 = false WHERE idJugador = @_idJugador";   
 
-            }
-        }
-        public static void saltearPregunta(Pregunta idPregunta)
-        {
-            if (_comodinSaltearPregunta==true)
-            {
-                
-                using (SqlConnection bd = new SqlConnection(_connectionString))
-                {
-                    string sql = "UPDATE jugador SET ComodinSaltear = false WHERE idJugador = @_idJugador";
-                    
-                }
-            }
-        }
-        public static jugador devloverJugador()
-        {
-            return _player;
-        }*/
+                 }
+
+
+
+
+             }
+         }
+         public static void saltearPregunta(Pregunta idPregunta)
+         {
+             if (_comodinSaltearPregunta==true)
+             {
+
+                 using (SqlConnection bd = new SqlConnection(_connectionString))
+                 {
+                     string sql = "UPDATE jugador SET ComodinSaltear = false WHERE idJugador = @_idJugador";
+
+                 }
+             }
+         }
+         public static jugador devloverJugador()
+         {
+             return _player;
+         }*/
     }
 }
